8022ba9f8400697cbb13aa68f749d0eee8c301a7
abrar997 <abraralrawi997.com>
2022-08-24 21:21:16 +0300

    spinner

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+// components
 import {
   Navbar,
   Header,
@@ -12,22 +13,52 @@ import {
   FindUs,
   Footer,
 } from "./component";
+import { HashLoader } from "react-spinners";
+const style={
+  background:"black"
+}
 const App = () => {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+  }, []);
+
   return (
-    <div>
-      <Navbar />
-      <Header />
-      <About />
-      {/* problem of small devices here in special menu don't forget it  */}
-      <SpecialMenu />
-      {/* -------------------------------------------------------------- */}
-      <Chef />
-      <Intro />
-      <Laurels />
-      <Gallery />
-      <FindUs />
-      <Footer />
-    </div>
+    <>
+      {loading ? (
+        <p
+          style={{
+            background: "black",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            width:"100%",
+            height:"900px"
+          }}
+        >
+          <HashLoader color="#D1B752" loading={loading}  size={100} />
+        </p>
+      ) : (
+        <div>
+          <Navbar />
+          <Header />
+          <About />
+          {/* problem of small devices here in special menu don't forget it  */}
+          <SpecialMenu />
+          {/* -------------------------------------------------------------- */}
+          <Chef />
+          <Intro />
+          <Laurels />
+          <Gallery />
+          <FindUs />
+          <Footer />
+        </div>
+      )}
+    </>
   );
 };
 
